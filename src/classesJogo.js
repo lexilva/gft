313e0772bafb8bab2e4d33fcde78f2d44ebb3b42
Alex Sandro <lexilva@gmail.com>
2024-11-03 15:29:41 -0300

    Construcão do método de atque do herói

--- src/classesJogo.js
+++ src/classesJogo.js
@@ -5,28 +5,24 @@ class hero{
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
+        this.ataque =""
     }
+    
     //Método de ataque do herói
     atacar(tipoDeHeroi){
 
-        let tipoDeAtaque = ""
-        switch (tipoDeAtaque){
-            case (tipoDeHeroi === "mago"):
-                tipoDeAtaque = "magia"
-                break
-            case(tipoDeHeroi === "guerreiro"):
-                tipoDeAtaque = "espada"
-                break
-            case(tipoDeHeroi === "monge"):
-                tipoDeAtaque ="artes marcias"
-                break
-            case(tipoDeHeroi === "ninja"):
-                tipoDeAtaque = "shuriken"
-                break
+        if (tipoDeHeroi =="mago"){
+            this.ataque = "magia"
+        }else if(tipoDeHeroi == "guerreiro"){
+             this.ataque = "espada"
+        }else if(tipoDeHeroi =="monge"){
+            this.ataque ="artes marcias"
+        }else if(tipoDeHeroi == "ninja"){
+            this.ataque = "shuriken"
         }
 
-        return console.log(`o ${tipoDeHeroi} atacou usando ${tipoDeAtaque}`)
+        return console.log(`o ${tipoDeHeroi} atacou usando ${this.ataque}`)
     }
 }
-let heroi = new hero("Alex",48,"guerreiro")
-heroi.atacar(hero.tipo)
+let heroi = new hero("Alex",48,"monge")
+heroi.atacar(heroi.tipo)
